cb6d243d15d96bb3372e4df23be0b34184691329
sleepanda4523 <63851752+sleepanda4523@users.noreply.github.com>
2020-07-28 11:44:09 +0900

    COMMIT

--- ALGOLA/11650_DIV2/sample.c
+++ ALGOLA/11650_DIV2/sample.c
@@ -7,36 +7,30 @@ typedef struct Coord {
 	int x;
 	int y;
 }coord;
-coord* bubble(coord*,int,int);
+int compare(const void* a, const void* b)    // 오름차순 비교 함수 구현
+{
+	int num1 = *(int*)a;    // void 포인터를 int 포인터로 변환한 뒤 역참조하여 값을 가져옴
+	int num2 = *(int*)b;    // void 포인터를 int 포인터로 변환한 뒤 역참조하여 값을 가져옴
+
+	if (num1 < num2)    // a가 b보다 작을 때는
+		return -1;      // -1 반환
+
+	if (num1 > num2)    // a가 b보다 클 때는
+		return 1;       // 1 반환
+
+	return 0;    // a와 b가 같을 때는 0 반환
+}
 int main()
 {
 	int size;
 	scanf("%d", &size);
 	coord* ndata = (coord*)malloc(sizeof(coord) * size);
 	for (int i = 0; i < size; i++) {
-		scanf("%d %d", ndata[i].x, ndata[i].y);
+		scanf("%d %d", &ndata[i].x, &ndata[i].y);
 	}
-	bubble(ndata,1,size);
+	qsort(ndata, size, sizeof(coord),compare);
+	for (int i = 0; i < size; i++) {
+		printf("%d %d\n", ndata[i].x, ndata[i].y);
+	}
 	return 0;
-}
-coord* bubble(coord* ndata, int check,int s)
-{
-	if (check == 1) {
-		for (int i = 0; i < s; i++) {
-			for (int j = 1; j < s - i; j++) {
-				if (ndata[j - 1].x > ndata[j].x) {
-					coord temp;
-					temp = ndata[j - 1];
-					ndata[j - 1] = ndata[j];
-					ndata[j] = temp;
-				}
-				else if (ndata[j].x == ndata[j - 1].x) {
-					bubble(ndata, check + 1, s);
-				}
-			}
-		}
-	}
-	else if (check == 2) {
-
-	}
 }
